9162947fc1a20d4440bfc93e6bd83516d6c8cb68
AkmalJ <akmaljaws08@gmail.com>
2022-12-15 06:09:04 +0700

    Deploy gh-pages

--- _next/static/chunks/webpack-b1051435cbcf8932.js
+++ _next/static/chunks/webpack-b1051435cbcf8932.js
@@ -121,7 +121,7 @@
 /******/ 		// This function allow to reference all chunks
 /******/ 		__webpack_require__.miniCssF = function(chunkId) {
 /******/ 			// return url for filenames based on template
-/******/ 			return "static/css/" + "be853a59b7ff4c65" + ".css";
+/******/ 			return "static/css/" + "3ed57ce9dd936010" + ".css";
 /******/ 		};
 /******/ 	}();
 /******/ 	
